45b1a8ccf841871d5f3c8abcdb3dacfa6e476d5f
bo <61472065+bwd202@users.noreply.github.com>
2023-09-22 16:44:37 -0400

    install html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: './src/index.js',
+    plugins: [
+
+        new HtmlWebpackPlugin({
+    
+          title: 'Book Wizard',
+    
+        }),
+    
+      ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+
+        rules: [
+
+            {
+
+                test: /\.css$/i,
+
+                use: ['style-loader', 'css-loader'],
+
+            },
+
+        ],
+
+    },
 };
