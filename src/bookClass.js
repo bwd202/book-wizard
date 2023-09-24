42951ed0f93e44655c1303d4f1ad3b514527a69e
bo <61472065+bwd202@users.noreply.github.com>
2023-09-24 18:22:23 -0400

    save

--- src/bookClass.js
+++ src/bookClass.js
@@ -1,13 +1,25 @@
 export {Book}
+import { myLibrary } from "./bookStorage"
 
 class Book {
+    
+  constructor({title = 'default', author = 'default', year = 1970, read = false} = {}) {
 
-    constructor({title = 'default', author = 'default', year = 1970, read = false} = {}) {
-  
-      this.title = title
-      this.author = author
-      this.year = year
-      this.read = read
-    }
-
+    this.title = title
+    this.author = author
+    this.year = year
+    this.read = read
   }
+
+  // get book() {
+  //   return this
+  // }
+
+  // static {
+    
+  //   let newBook = new Book(newBook.book)
+  //   myLibrary.push(newBook)
+  //   console.log(myLibrary)
+  // }
+
+}
