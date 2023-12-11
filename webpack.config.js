80f0dc3b8cadf7598e348b9b830da586ce41262b
bo <61472065+bwd202@users.noreply.github.com>
2023-12-11 11:10:12 -0500

    add font webpack loader
    add font file

--- webpack.config.js
+++ webpack.config.js
@@ -23,13 +23,13 @@ module.exports = {
 
         rules: [
             {
-
                 test: /\.css$/i,
-
                 use: ['style-loader', 'css-loader'],
-
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+              },
         ],
-
     },
 };
